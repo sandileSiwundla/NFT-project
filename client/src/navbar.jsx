2edb47129e0025fba132f64290e005731ce77617
sandileSiwundla <sandsmthembu2@gmail.com>
2025-09-14 23:53:45 +0200

    Navbar changed

--- client/src/navbar.jsx
+++ client/src/navbar.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
+
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-brand">Umkhonto</div>
-      <ul className="navbar-links">
+      <div
+        className="navbar-toggle"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        ☰
+      </div>
+      <ul className={`navbar-links ${isOpen ? 'active' : ''}`}>
         <li><a href="#create">Create NFT</a></li>
         <li><a href="#look-up">Look Up NFT</a></li>
         <li><a href="#buy">Buy NFT</a></li>
